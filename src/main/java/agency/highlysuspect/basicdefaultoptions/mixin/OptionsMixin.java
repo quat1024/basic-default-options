2e4864da0797acb7a200dcf83b554c12737d7f59
quat1024 <quat1024@users.noreply.github.com>
2022-06-09 20:51:47 -0400

    Simpler

--- src/main/java/agency/highlysuspect/basicdefaultoptions/mixin/OptionsMixin.java
+++ src/main/java/agency/highlysuspect/basicdefaultoptions/mixin/OptionsMixin.java
@@ -1,40 +1,33 @@
 package agency.highlysuspect.basicdefaultoptions.mixin;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.Options;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Mixin(Options.class)
 public class OptionsMixin {
 	@Shadow @Final private File optionsFile;
-	@Unique private File defaultOptionsFile;
-	
-	@Inject(method = "<init>", at = @At(
-		value = "INVOKE", //BeforeInvoke
-		target = "Lnet/minecraft/client/Options;load()V"
-	))
-	private void onInit(Minecraft minecraft, File file, CallbackInfo ci) {
-		defaultOptionsFile = new File(file, "defaultoptions.txt");
-	}
 	
 	@Inject(method = "load", at = @At("HEAD"))
 	private void onLoad(CallbackInfo ci) {
 		try {
-			if(defaultOptionsFile.exists() && !optionsFile.exists()) {
-				System.out.println("Copying default options");
-				Files.copy(defaultOptionsFile.toPath(), optionsFile.toPath());
+			Path optionsPath = optionsFile.toPath();
+			Path defaultOptionsPath = optionsPath.resolveSibling("defaultoptions.txt");
+			
+			if(!Files.exists(optionsPath) && Files.exists(defaultOptionsPath)) {
+				System.out.println("[basic-default-options] Copying default options");
+				Files.copy(defaultOptionsPath, optionsPath);
 			}
 		} catch (Exception e) {
-			System.err.println("[simple-default-options] Trouble");
+			System.err.println("[basic-default-options] Trouble");
 			e.printStackTrace();
 		}
 	}
